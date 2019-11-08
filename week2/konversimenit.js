6a371b67736e469befc2cb40c585a86a3190a333
daniarzaq <mdania27@gmail.com>
2019-11-08 21:30:27 +0700

    Week2_all

---
+++ week2/konversimenit.js
@@ -0,0 +1,25 @@
+function konversiMenit(menit) {
+    
+    menit = a/60;
+    menit = Math.floor(menit)
+
+        z = menit*60
+        digit2 = menit - z
+        digit2 = digit2.toString()
+
+            if (digit2.length < 2) {
+                digit2 = '0' + digit2
+            }
+
+            jam = a + ':' + digit2;
+
+    
+    return jam
+  }
+  
+ 
+  console.log(konversiMenit(63)); // 1:03
+  console.log(konversiMenit(124)); // 2:04
+  console.log(konversiMenit(53)); // 0:53
+  console.log(konversiMenit(88)); // 1:28
+  console.log(konversiMenit(120)); // 2:00
